0ef887931e7806d51e4aaa9496e72bd58dad8e2a
margheritamanzi03-stack <margherita.manzi03@gmail.com>
2026-04-03 17:31:37 +0200

    Aggiunto test - da finire

--- diadia/test/diadia/BorsaTest.java
+++ diadia/test/diadia/BorsaTest.java
@@ -8,15 +8,49 @@ import org.junit.jupiter.api.Test;
 class BorsaTest {
 	
 	private Borsa borsa; 
+	private Attrezzo libro; 
+	private Attrezzo cassapanca;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		this.borsa = new Borsa(); 
+		this.borsa = new Borsa(11);
+		this.libro = new Attrezzo("libro",1); 
+		this.cassapanca = new Attrezzo("cassapanca",12); 
+		
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAddAttrezzoNull() {
+		assertFalse(borsa.addAttrezzo(null));
+	}
+	
+	@Test
+	void testAddAttrezzoConSuccesso() {
+		assertTrue(borsa.addAttrezzo(libro));
+	}
+	
+	@Test
+	void TestAddAttrezzoPesoMaxRaggiunto() {
+		assertFalse(borsa.addAttrezzo(cassapanca)); 
+		
+	}
+	
+	@Test
+	void testAddAttrezzoNumeroMaXDiAttrezziRaggiunto() {
+		for(int i=0; i<10; i++)
+			borsa.addAttrezzo(libro); 
+		assertFalse(borsa.addAttrezzo(libro)); 
+	}
+	
+	@Test
+	void testGetPesoMaxStandard() {
+		Borsa borsa_standard = new Borsa(); 
+		assertEquals(borsa_standard.getPesoMax(),10);
+	}
+	
+	@Test 
+	void testGetPesoMaxNonStandard() {
+		assertEquals(borsa.getPesoMax(),11);
 	}
 
 }
