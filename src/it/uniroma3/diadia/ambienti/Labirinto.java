b2a529a8f8a29ef1b1ad1439e61c2f863622f447
margheritamanzi03-stack <margherita.manzi03@gmail.com>
2026-04-04 19:49:45 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/ambienti/Labirinto.java
@@ -0,0 +1,72 @@
+package it.uniroma3.diadia.ambienti;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+/**
+ * Questa classe modella il labirinto per la partita
+ * @author: Margherita Manzi e Niccolò Ilari
+ * @see Stanza
+ * @versione base 
+ */
+public class Labirinto {
+	
+	private Stanza entrata; 
+	private Stanza uscita; 
+	
+	public Labirinto() {
+		init();
+	}
+	
+	/**
+	 * Inizializza il labirinto per la partita
+	 */
+	private void init() {
+		/* crea gli attrezzi */
+    	Attrezzo lanterna = new Attrezzo("lanterna",3);
+		Attrezzo osso = new Attrezzo("osso",1);
+    	
+		/* crea stanze del labirinto */
+		Stanza atrio = new Stanza("Atrio");
+		Stanza aulaN11 = new Stanza("Aula N11");
+		Stanza aulaN10 = new Stanza("Aula N10");
+		Stanza laboratorio = new Stanza("Laboratorio Campus");
+		Stanza biblioteca = new Stanza("Biblioteca");
+		
+		/* collega le stanze */
+		atrio.impostaStanzaAdiacente("nord", biblioteca);
+		atrio.impostaStanzaAdiacente("est", aulaN11);
+		atrio.impostaStanzaAdiacente("sud", aulaN10);
+		atrio.impostaStanzaAdiacente("ovest", laboratorio);
+		aulaN11.impostaStanzaAdiacente("est", laboratorio);
+		aulaN11.impostaStanzaAdiacente("ovest", atrio);
+		aulaN10.impostaStanzaAdiacente("nord", atrio);
+		aulaN10.impostaStanzaAdiacente("est", aulaN11);
+		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
+		laboratorio.impostaStanzaAdiacente("est", atrio);
+		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
+		biblioteca.impostaStanzaAdiacente("sud", atrio);
+
+        /* pone gli attrezzi nelle stanze */
+		aulaN10.addAttrezzo(lanterna);
+		atrio.addAttrezzo(osso);
+
+		// il gioco comincia nell'atrio
+        entrata = atrio;  
+		uscita = biblioteca;
+	}
+	
+	/**
+	 * Ritorna l'entrata
+	 * @return Stanza
+	 */
+	public Stanza getEntrata() {
+		return entrata;
+	}
+	/**
+	 * Ritorna l'uscita
+	 * @return Stanza
+	 */
+	public Stanza getUscita() {
+		return uscita;
+	}
+}
---
+++ src/it/uniroma3/diadia/giocatore/Borsa.java
@@ -0,0 +1,135 @@
+package it.uniroma3.diadia.giocatore;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+/**
+ * Classe che gestisce la borsa del giocatore e i suoi atrezzi
+ * @author: Margherita Manzi e Niccolo Ilari
+ * @see: Giocatore, Attrezzo
+ * @versio: base
+ */
+public class Borsa {
+	
+	public final static int DEFAULT_PESO_MAX_BORSA = 10;
+	private Attrezzo[] attrezzi;
+	private int numeroAttrezzi;
+	private int pesoMax;
+	
+	public Borsa() {
+		this(DEFAULT_PESO_MAX_BORSA);
+	}
+	
+	public Borsa(int pesoMax) {
+		this.pesoMax = pesoMax;
+		this.attrezzi = new Attrezzo[pesoMax]; // modifica qui
+		this.numeroAttrezzi = 0;
+	}
+	
+	/**
+	 * Aggiunge un attrezzo alla borsa e restituisce true se l'operazione è andata a buon fine
+	 * @param attrezzo
+	 * @return boolean
+	 */
+	public boolean addAttrezzo(Attrezzo attrezzo) {
+		if (attrezzo==null) 
+			return false; 
+		if (this.getPeso() + attrezzo.getPeso() > this.getPesoMax())
+				return false;
+		
+		if (this.numeroAttrezzi==this.getPesoMax()) //mpdifica qui
+				return false;
+		this.attrezzi[this.numeroAttrezzi] = attrezzo;
+		this.numeroAttrezzi++;
+		return true;
+	}
+	
+	/**
+	 * Restituisce il peso massimo della borsa
+	 * @return int
+	 */
+	public int getPesoMax() {
+		return pesoMax;
+	}
+	
+	/**
+	 * Restituisce l'attrezzo cercato, se presente, altrimenti restituisce null
+	 * @param nomeAttrezzo
+	 * @return attrezzo cercato
+	 */
+	public Attrezzo getAttrezzo(String nomeAttrezzo) {
+		Attrezzo a = null;
+		for (int i= 0; i<this.numeroAttrezzi; i++)
+			if (this.attrezzi[i].getNome().equals(nomeAttrezzo))
+				a = attrezzi[i];
+
+		return a;
+	}
+	
+	/**
+	 * Restituisce il peso attuale della borsa
+	 * @return peso
+	 */
+	public int getPeso() {
+		int peso = 0;
+		for (int i= 0; i<this.numeroAttrezzi; i++)
+		peso += this.attrezzi[i].getPeso();
+
+		return peso;
+	}
+	
+	/**
+	 * Verifica se la borsa sia vuota o no
+	 * @return boolean
+	 */
+	public boolean isEmpty() {
+		return this.numeroAttrezzi == 0;
+	}
+	
+	/**
+	 * Verfica se un attrezzo sia presente o meno nella borsa
+	 * @param nomeAttrezzo
+	 * @return boolean
+	 */
+	public boolean hasAttrezzo(String nomeAttrezzo) {
+		return this.getAttrezzo(nomeAttrezzo)!=null;
+	}
+		
+	/**
+	 * Rimuove atrezzo dalla borsa
+	 * @param nomeAttrezzo
+	 * @return attrezzo rimosso
+	 */
+	public Attrezzo removeAttrezzo(String nomeAttrezzo) {
+		Attrezzo a = null;
+		for (int i=0; i<this.numeroAttrezzi; i++) {
+			if(this.attrezzi[i].getNome().equals(nomeAttrezzo)) {
+				a=this.attrezzi[i];
+				this.attrezzi[i]=null;
+				this.numeroAttrezzi--;
+				for(int j=i+1; j<this.numeroAttrezzi; j++) {
+					this.attrezzi[j-1]=this.attrezzi[j]; 
+				}
+				break;
+			}
+		}
+		return a;
+	}
+	
+	
+	/**
+	 * Descrizione della borsa	
+	 */
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+
+		if (!this.isEmpty()) {
+			s.append("Contenuto borsa ("+this.getPeso()+"kg/"+this.getPesoMax()+"kg): ");
+			for (int i= 0; i<this.numeroAttrezzi; i++)
+				s.append(attrezzi[i].toString()+" ");
+		}
+		else
+			s.append("Borsa vuota");
+		return s.toString();
+	}
+
+}
