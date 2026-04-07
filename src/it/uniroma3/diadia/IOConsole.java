945669d9854dd1779facd74b434641e6f365c39b
margheritamanzi03-stack <margherita.manzi03@gmail.com>
2026-04-07 10:37:36 +0200

    Add JavaDoc comments to IOConsole class
    Added JavaDoc comments to the IOConsole class and its methods.

--- src/it/uniroma3/diadia/IOConsole.java
+++ src/it/uniroma3/diadia/IOConsole.java
@@ -1,11 +1,27 @@
 package it.uniroma3.diadia;
 import java.util.Scanner;
 
+/**
+ * Classe che gestisce la console di Input/Output del gioco
+ * 
+ * @author Margherita Manzi e Niccolò Ilari
+ * @versione base
+ */
+
 public class IOConsole {
+	
+	/**
+	 * Stampa a a schermo la stringa ricevuta 
+	 * @param msg
+	 */
 	public void mostraMessaggio(String msg) {
 		System.out.println(msg);
 	}
 	
+	/**
+	 * Legge e memorizza l'input 
+	 * @return rigaInput
+	 */
 	public String leggiRiga() {
 		Scanner scannerDiLinee = new Scanner(System.in);
 		String riga = scannerDiLinee.nextLine();
